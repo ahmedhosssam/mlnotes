f0471641c31682c4f159bd20b2747e9db757dbd4
Ahmed Hossam <hoss4614@gmail.com>
2024-11-13 00:42:19 +0200

    try george hotz style of code

--- k_means.py
+++ k_means.py
@@ -31,34 +31,18 @@ Repeat until convergence:
 '''
 
 def get_closest_centroid(x, y):
-    min_d = 1e9
-    c = 0
-    for j in range(clust_n):
-        d = (math.pow(cluster_x[j]-x, 2)+math.pow(cluster_y[j]-y, 2))
-        if d < min_d:
-            min_d = d
-            c = j
+    c, min_d = min(((j, (cluster_x[j] - x)**2 + (cluster_y[j] - y)**2) for j in range(clust_n)), key=lambda item: item[1])
     return c
 
 def get_mean(j, axis):
-    den = 0
-    nom = 0
-    if axis == 'x':
-        den = np.count_nonzero(C==j)
-        nom = np.sum(X[C==j])
-    else:
-        den = np.count_nonzero(C==j)
-        nom = np.sum(Y[C==j])
+    den, nom = np.count_nonzero(C==j), np.sum((X if axis == 'x' else Y)[C==j])
     return nom/den if den != 0 else 0
 
 for g in range(10000): # It should be: Repeat until convergence
-    for i in range(C.size):
-        # get the closest cluster centroid.
-        C[i] = get_closest_centroid(X[i], Y[i])
-
+    # get the closest cluster centroid. 
+    for i in range(C.size): C[i] = get_closest_centroid(X[i], Y[i])
     for j in range(clust_n):
-        cluster_x[j] = get_mean(j, 'x')
-        cluster_y[j] = get_mean(j, 'y')
+        cluster_x[j], cluster_y[j] = get_mean(j, 'x'), get_mean(j, 'y')
 
 plt.figure(figsize=(8, 6))
 plt.scatter(X, Y, c='blue', alpha=0.6, edgecolors='k')
